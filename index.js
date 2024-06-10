bcc8904a682378795d792e513dd1fa0ddb1b5f8e
Faleye Olumide Hakeem <faleyehakeem@yahoo.com>
2024-06-10 08:36:26 -0400

    recreat

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import hotel from "./hotel.js";
+import Cors from "cors";
 
 const app = express();
 
+app.use(Cors({ origin: "*"})
+const allData = [...hotel]
 const port = 4000;
 
+
 app.get("/", (req, res) => {
-  res.send(hotel);
+  res.send(allData);
 });
 
 app.listen(port, () => {
