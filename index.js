e9a032b1249d1b21dfe58661187c87f749da101f
Faleye Olumide Hakeem <faleyehakeem@yahoo.com>
2024-06-09 20:24:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("welcome to backend");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
